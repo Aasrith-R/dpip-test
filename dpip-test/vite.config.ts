4d5724e69603225cc6080b44a2421c37265a3bee
Aasrith-R <166086806+Aasrith-R@users.noreply.github.com>
2026-01-18 17:45:31 -0500

    Add base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- dpip-test/vite.config.ts
+++ dpip-test/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dpip-test/',   // <-- REQUIRED for GitHub Pages repo deployment
   plugins: [
     vue(),
     vueDevTools(),
